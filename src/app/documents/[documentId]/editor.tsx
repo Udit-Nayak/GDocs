460037bf52b6d7377886cecc6f7d8fa7ab86253d
Udit <uditnayak1611@gmail.com>
2025-08-08 02:02:15 +0530

    ruler added

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -19,6 +19,7 @@ import TableHeader from "@tiptap/extension-table-header";
 import { useEditorStore } from "@/store/use-editor-store";
 import { FontSizeExtension } from "@/extensions/font-size"; 
 import { LineHeightExtension } from "@/extensions/line-height";
+import { Ruler } from "./ruler";
 
 
 
@@ -26,6 +27,7 @@ import { LineHeightExtension } from "@/extensions/line-height";
 export const Editor = () => {
   const { setEditor } = useEditorStore();
   const editor = useEditor({
+    immediatelyRender:false,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -54,7 +56,7 @@ export const Editor = () => {
     editorProps: {
       attributes: {
         style:
-          "padding-left:56px; padding-right:56px; padding-top: 56px; padding-bottom: 56px;",
+          "padding-left:56px; padding-right:56px;",
         class:
           "focus:outline-none print:border-0  bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px]  pt-10 pr-14 pb-10 cursor-text",
       },
@@ -110,6 +112,7 @@ export const Editor = () => {
   });
   return (
     <div className="size-full overflow-x-auto bd-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <Ruler />
       <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
         <EditorContent editor={editor} />
       </div>
