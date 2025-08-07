6346fb177ac996229ad6c0bd6ea8987ea5756019
Udit <uditnayak1611@gmail.com>
2025-08-07 22:05:58 +0530

    Toolbar

---
+++ src/app/documents/page.tsx
@@ -0,0 +1,9 @@
+const DocumentsPage = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+        Documents Page
+    </div>
+  );
+}
+
+export default DocumentsPage;
